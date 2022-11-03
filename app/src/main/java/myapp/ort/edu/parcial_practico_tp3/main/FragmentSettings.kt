f77aaf6daadeb2d92f281ad45a3047cf94ffa92b
Danielbsas <danielpisa@gmail.com>
2022-11-03 18:35:03 -0300

    diseño settings

--- app/src/main/java/myapp/ort/edu/parcial_practico_tp3/main/FragmentSettings.kt
+++ app/src/main/java/myapp/ort/edu/parcial_practico_tp3/main/FragmentSettings.kt
@@ -5,29 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import myapp.ort.edu.parcial_practico_tp3.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentSettings.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentSettings : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var tVSettNightMode: TextView
+    private lateinit var tVBuscadorOnOff: TextView
+    private lateinit var tVFavoritosOnOff: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +23,16 @@ class FragmentSettings : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+        tVSettNightMode = view.findViewById(R.id.textViewSettNightMode)
+        tVBuscadorOnOff = view.findViewById(R.id.textViewBuscadorOnOff)
+        tVFavoritosOnOff = view.findViewById(R.id.textViewFavoritosOnOff)
+
+
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentSettings.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentSettings().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
