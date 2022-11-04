353c2b9d2fad4d906009963744ec2701b4e07a25
Juan Pablo Forno <fornojp@gmail.com>
2022-11-03 21:27:42 -0300

    fragmentSettings

--- app/src/main/java/myapp/ort/edu/parcial_practico_tp3/main/FragmentSettings.kt
+++ app/src/main/java/myapp/ort/edu/parcial_practico_tp3/main/FragmentSettings.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import myapp.ort.edu.parcial_practico_tp3.R
 
 class FragmentSettings : Fragment() {
@@ -28,11 +30,89 @@ class FragmentSettings : Fragment() {
         tVBuscadorOnOff = view.findViewById(R.id.textViewBuscadorOnOff)
         tVFavoritosOnOff = view.findViewById(R.id.textViewFavoritosOnOff)
 
+        val sharedPreferences =
+            activity?.getSharedPreferences("SETTINGS_DATA", AppCompatActivity.MODE_PRIVATE)
 
+        var nightMode = sharedPreferences?.getBoolean("nightMode", false) == true
+        toggleNightMode(nightMode)
+        var buscador = sharedPreferences?.getBoolean("buscador", false) == true
+        toggleBuscador(buscador)
+        var favoritos = sharedPreferences?.getBoolean("favoritos", false) == true
+        toggleFavoritos(favoritos)
+
+        tVSettNightMode.setOnClickListener {
+            nightMode = sharedPreferences?.getBoolean("nightMode", false) == true
+            val settingsData = sharedPreferences?.edit()
+            settingsData?.putBoolean("nightMode", !nightMode)
+            settingsData?.apply()
+            toggleNightMode(!nightMode)
+            Toast.makeText(
+                activity?.applicationContext,
+                "$nightMode",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        tVBuscadorOnOff.setOnClickListener {
+            buscador = sharedPreferences?.getBoolean("buscador", false) == true
+            val settingsData = sharedPreferences?.edit()
+            settingsData?.putBoolean("buscador", !buscador)
+            settingsData?.apply()
+            toggleBuscador(!buscador)
+            Toast.makeText(
+                activity?.applicationContext,
+                "$buscador",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        tVFavoritosOnOff.setOnClickListener {
+            favoritos = sharedPreferences?.getBoolean("favoritos", false) == true
+            val settingsData = sharedPreferences?.edit()
+            settingsData?.putBoolean("favoritos", !favoritos)
+            settingsData?.apply()
+            toggleFavoritos(!favoritos)
+            Toast.makeText(
+                activity?.applicationContext,
+                "$favoritos",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        Toast.makeText(
+            activity?.applicationContext,
+            "No completaste con tus datos!",
+            Toast.LENGTH_SHORT
+        ).show()
         return view
     }
 
-    companion object {
-
+    private fun toggleNightMode(nightMode: Boolean) {
+        if (nightMode) {
+            tVSettNightMode.setText(getString(R.string.sett_day_mode))
+        } else {
+            tVSettNightMode.setText(getString(R.string.sett_night_mode))
+        }
     }
+
+    private fun toggleBuscador(buscador: Boolean) {
+        if (buscador) {
+            tVBuscadorOnOff.setText(getString(R.string.sett_buscador_off))
+        } else {
+            tVBuscadorOnOff.setText(getString(R.string.sett_buscador_on))
+        }
+    }
+
+    private fun toggleFavoritos(favoritos: Boolean) {
+        if (favoritos) {
+            tVFavoritosOnOff.setText(getString(R.string.sett_favoritos_off))
+        } else {
+            tVFavoritosOnOff.setText(getString(R.string.sett_favoritos_on))
+        }
+    }
+
+    companion object {
+        fun newInstance(): FragmentSettings {
+            return FragmentSettings()
+                   }
+            }
 }
